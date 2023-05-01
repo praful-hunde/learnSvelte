1ea0dc0c507e801ae552922ff2141cbf961fe083
Prafulla Hunde - Lenze <36070180+prafullahunde@users.noreply.github.com>
2023-05-01 16:47:20 +0530

    Added example for styling.

---
+++ src/routes/Exercises/Styling/+page.svelte
@@ -0,0 +1,11 @@
+<p>This is a paragraph.</p>
+The style is always scoped to the component in svelte.
+
+<!--The style are scoped to the component -->
+ <style> 
+    p{
+        color: #000;
+        font-family: cursive;
+        font-size: 2em;
+    }
+</style>
