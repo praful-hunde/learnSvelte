956e83140b8f4ea1191a0938a0815a8286b7a846
Prafulla Hunde - Lenze <36070180+prafullahunde@users.noreply.github.com>
2023-05-02 21:22:32 +0530

    added reactive declaration,statement,block

---
+++ src/routes/Exercises/Declaration/+page.svelte
@@ -0,0 +1,28 @@
+<h1>Reactive Declaration</h1>
+
+
+<p>count = {count}</p>
+<p>In line double count = {count * 2}</p>
+<p>count double = {countDouble}</p>
+
+<button on:click={()=>{++count}}>Increment count</button>
+
+
+<script lang="ts">
+    let count:number = 0 ;
+    let countDouble:number = 0;
+    
+    // Reactive declaration
+   $: countDouble = count*2;
+
+// Reactive statement 
+
+$: console.log("Reactive statement " + count);
+
+$: {
+
+    alert('Reactive block ' + count);
+}
+
+
+</script>
