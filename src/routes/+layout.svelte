e1813035c01be627572191de4d35ee64a7787dfd
Prafulla <praful.hunde@gmail.com>
2023-06-16 21:29:07 +0000

    Added and innitialzied @vercel/analytics package

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,10 @@
 	</ul>
 </nav>
 <slot />
+
+<script>
+	import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
+</script>
